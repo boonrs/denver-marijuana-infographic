9f78d69d88ac3cfdbcc0ede839a6f5d2497d6bdd
KHarshawat <kharshawat@gmail.com>
2014-08-11 15:23:22 -0700

    re-adds infographic border, removes console logs

--- javascripts/scrollshadow.js
+++ javascripts/scrollshadow.js
@@ -4,11 +4,9 @@ app.hookupScrollShadow = function() {
   var  ns = "nav-shadow";
 
   $(window).scroll(function() {
-    console.log($(this).scrollTop())
-    console.log(mn, ns)
+
     if( $(this).scrollTop() > 80 ) {
       mn.addClass(ns);
-
     } else {
       mn.removeClass(ns);
     }
