9af9351ab5049d5836bf5ba8d86d277dfe8c471c
Becky Boone <becky.sue.boone@gmail.com>
2014-08-12 11:37:38 -0700

    Add crime from open data portal

--- javascripts/crime.js
+++ javascripts/crime.js
@@ -5,9 +5,38 @@ $( document ).ready(function() {
     "description_bold": "... marijuana related crimes have been reported this year.",
     "description": "This is the sum total of Industry and Nonindustry."
   };
-  crimeCount(big_number);
-  crimeOffenses();
+  // crimeCount(big_number);
+  // crimeOffenses();
+  crimePossession();
 });
+
+function crimePossession() {
+  var svg = dimple.newSvg("#crime-possession", 590, 600);
+  d3.csv("data/open-crime.csv", function (data) {
+    var myChart = new dimple.chart(svg, data);
+    myChart.setBounds(60, 30, 510, 330)
+    var x = myChart.addCategoryAxis("x", "Quarter" );
+    x.addOrderRule("year");
+    x.addOrderRule("q");
+    myChart.addMeasureAxis("y", "Total");
+    var bars = myChart.addSeries("Series", dimple.plot.bar);
+
+    // Tooltip
+    bars.getTooltipText = function (e) {
+      return [e.aggField[0] + ' ' + e.cx, e.cy];
+    };
+
+    // Styling: Change this Kavi!
+    myChart.defaultColors = [
+      new dimple.color("#e5e9ea"),
+      new dimple.color("#8eb5bc"),
+      new dimple.color("#287f93"),
+      new dimple.color("#154651")
+    ];
+    myChart.addLegend("20%,20px","1%,20px","10%,20px","10%,20px");
+    myChart.draw();
+  });
+}
 
 function crimeOffenses() {
   var svg = dimple.newSvg("#crime-stacked", 590, 400);
